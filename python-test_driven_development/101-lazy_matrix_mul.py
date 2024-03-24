cd3aadf419403986e374ad4d2376a03e0fcd86b9
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2024-03-24 18:20:21 -0400

    test

---
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the lazy_matrix_mul module.
+This module supplies one function, lazy_matrix_mul().
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    just a function
+    using a module
+    """
+    return np.matmul(m_a, m_b)
