ccd6fe3c8c029da22fb5f65e468b8f1e1007747e
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2024-03-24 16:21:48 -0400

    bbbnmvmnvn

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""
-Task 0:
-Fetch https://intranet.hbtn.io/status
+'''a Python script that fetches a url'''
 
-0-hbtn_status.py
-"""
 
-import urllib
+import urllib.request
 
-if __name__ == "__main__":
-    try:
-        req = urllib.request.Request('https://intranet.hbtn.io/status')
-        with urllib.request.urlopen(req) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode('utf-8')))
-    except urllib.error.URLError as e:
-        print("Failed to fetch URL:", e.reason)
-    except Exception as e:
-        print("Error:", e)
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
