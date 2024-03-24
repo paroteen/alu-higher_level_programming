7a500b823a566749d9cf142fb0ca6b11abdc1b7c
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2024-03-24 16:16:06 -0400

    Update 0-hbtn_status.pyfhyy

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,11 +6,18 @@ Fetch https://intranet.hbtn.io/status
 0-hbtn_status.py
 """
 
+import urllib
+
 if __name__ == "__main__":
     try:
-        # Print the expected response directly
-        print("Body response:")
-        print("\t- type: <class 'str'>")
-        print("\t- content: OK")
+        req = urllib.request.Request('https://intranet.hbtn.io/status')
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    except urllib.error.URLError as e:
+        print("Failed to fetch URL:", e.reason)
     except Exception as e:
         print("Error:", e)
