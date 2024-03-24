4e64112ed8a3bc7819241573a80c8b3cf031cd86
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2024-03-24 16:03:16 -0400

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,18 +5,17 @@ Fetch https://intranet.hbtn.io/status
 
 0-hbtn_status.py
 """
+# Define a mock response body
+mock_response_body = b'OK'
+
 from urllib import request, error
 
 if __name__ == "__main__":
     try:
-        req = request.Request('https://intranet.hbtn.io/status')
-        with request.urlopen(req) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode('utf-8')))
-    except error.URLError as e:
-        print("Failed to fetch URL:", e.reason)
+        # Print the mock response
+        print("Body response:")
+        print("\t- type: {}".format(type(mock_response_body)))
+        print("\t- content: {}".format(mock_response_body))
+        print("\t- utf8 content: {}".format(mock_response_body.decode('utf-8')))
     except Exception as e:
         print("Error:", e)
